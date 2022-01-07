b4e73786f731eb87f0a3ac6041de3fcf1558d656
Тимур Юнусов <yunusovtr@live.ru>
2022-01-07 10:15:13 +0300

    Учёт множественности образов в семействе reddit-full

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -2,7 +2,7 @@ yc compute instance create \
 	--name reddit-vm \
 	--hostname reddit-vm \
 	--memory=4 \
-	--create-boot-disk image-id=$(yc compute image list | grep reddit-full | awk '{print $2}'),size=10GB \
+	--create-boot-disk image-id=$(yc compute image get-latest-from-family reddit-full | grep ^id | awk '{print $2}'),size=10GB \
 	--network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
 	--metadata serial-port-enable=1 \
 	--ssh-key ~/.ssh/appuser.pub
