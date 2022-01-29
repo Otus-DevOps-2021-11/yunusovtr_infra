3437109421cf512ff49897ca0cac22a3861f6b53
Тимур Юнусов <yunusovtr@live.ru>
2022-01-29 17:21:37 +0300

    Динамические инвентори для окружений

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -10,6 +10,18 @@ output "internal_ip_address_db" {
   value = module.db.internal_ip_address_db
 }
 
+# Генерируем инвентори соответствующего-окружения
+resource "local_file" "ansible_inventory" {
+  content = templatefile("inventory.tftpl",
+    {
+      external_ip_address_app = module.app.external_ip_address_app[0],
+      external_ip_address_db = module.db.external_ip_address_db,
+      internal_ip_address_db = module.db.internal_ip_address_db,
+    }
+  )
+  filename = "../../ansible/environments/${var.environment}/inventory"
+}
+
 # output "external_ip_load_balancer" {
 #   value = yandex_lb_network_load_balancer.reddit-app-network-lb.listener.*.external_address_spec[0].*.address[0]
 # }
